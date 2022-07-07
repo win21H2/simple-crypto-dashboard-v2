cc975ea8124c42e6dc5dfbdda17642d05a736e0c
324Hz <mark@cosimo.fund>
2022-07-07 11:00:39 -0500

    commit

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Coin = ({ image, symbol, price, priceChange, high24h, low24h, lastupdated }) => {
-  
+const Coin = ({ image, symbol, price, priceChange, high24h, low24h, lastupdated, name, maxsupply, totalsupply, circsupply }) => {
+
   return (
     <>
     <div id="CRYPTO--CURRENT">
@@ -10,14 +10,19 @@ const Coin = ({ image, symbol, price, priceChange, high24h, low24h, lastupdated 
           <div className="coin-data">
             <pre>
             <div className="coin">
-              <a href={image} target="_blank" rel="noreferrer" className="cursor-crosshair"><img src={image} alt=""/></a><br/>
+              <a href={image} target="_blank" rel="noreferrer" id="coin-imageborder" className="cursor-crosshair"><img src={image} alt=""/></a><br/>
             </div>
             <div className="coin-symbol">Symbol: <b>{symbol}</b></div>
-            <p className="coin-price">Price: ${price}</p>
+            <div className="coin-name">Name: <b>{name}</b></div>
+            <p className="coin-price">Current price: ${price}</p>
             <p className="coin-high24h">High (24h): ${high24h}</p>
             <p className="coin-low24h">Low (24h): ${low24h}</p>
             {priceChange < 0 ? (<p className="coin-percent red">Change (24h): {priceChange.toFixed(1)}%</p>) : (<p className="coin-percent green">Change (24h): {priceChange.toFixed(1)}%</p>)}
-            <p className="coin-lastupdated">Last updated  {lastupdated}</p>
+            <p className="">Max supply: {maxsupply}</p>
+            <p className="">Total supply: {totalsupply}</p>
+            <p className="">Circulating supply: {circsupply}</p>
+            <p className="coin-lastupdated">Last updated {lastupdated}</p>
+            <br/>
             </pre>
           </div>
         </div>
--- src/components/CoinFormat.js
+++ src/components/CoinFormat.js
@@ -40,51 +40,72 @@ const CoinFormat = () => {
         const sortedCoins = filteredCoins.sort((a, b) => { return a.price_change_percentage_24h - b.price_change_percentage_24h; });
         setCoins(sortedCoins);
     };
-
+    // const checkMaxSupply = () => {
+    //     filteredCoins.forEach((coin) => {
+    //         if (coin.max_supply === null) {
+    //             console.log(coin.name + " has no max supply!");
+    //         }
+    //     }
+    // )};
+    const checkPrice = () => {
+        filteredCoins.forEach((coin) => {
+            if (coin.symbol === "eth") {
+                if (coin.current_price > 1000) {
+                    console.log(coin.name + " is over 1000!");
+                    // check if the price is getting close to the low_24h price if so, change the color to red
+                }
+                // check if the price is inbetween high_24h and low_24h if so, change the color to orange/yellow
+                // try to figure out how to make it change color based on how close it is to the high or low values
+                if (coin.current_price === coin.low_24h) {
+                    console.log(coin.name + " is under 1000!");
+                    // check if the price is getting close to the high_24h price if so, change the color to green
+                }
+            }
+        }
+    )};
+    checkPrice();
+    // checkMaxSupply();
     return (
         <div>
-                <div className="coin-app">
-                    <div className="coin-search">
-                        <div className="coin-HTLLTH">
-                            <h4>Filter</h4>
-                            <pre>
-                                <button onClick={HTLprice} className="sortbutton">high to low (price $USD)</button><br/><br/>
-                                <button onClick={LTHprice} className="sortbutton">low to high (price $USD)</button>
-                                <br/><br/><br/>
-                                <button onClick={HTLchange} className="sortbutton">high to low (change %)</button><br/><br/>
-                                <button onClick={LTHchange} className="sortbutton">low to high (change %)</button>
-                            </pre>
-                        </div>
-                        <br/>
-                        <br/>
-                        <div className="coin-boundbox">
-                            {filteredCoins.map((coin) => {
-                                return (
-                                    <>
-                                    <Coin
-                                        key={coin.id}
-                                        image={coin.image}
-                                        symbol={coin.symbol}
-                                        price={coin.current_price}
-                                        priceChange={coin.price_change_percentage_24h}
-                                        high24h={coin.high_24h}
-                                        low24h={coin.low_24h}
-                                        lastupdated={coin.last_updated}
-                                    />
-                                    </>
-                                );
-                            })}
-                            {/* COPY AND PASTE THE COIN DATA AND TRANSFORM IT SO THAT */}
-                        </div>
-                        <br/>
-                        <br/>
+            <div className="coin-app">
+                <div className="coin-search">
+                    <div className="coin-HTLLTH">
+                        <h4>Filter</h4>
+                        <pre>
+                            <button onClick={HTLprice} className="sortbutton">high to low (price $USD)</button><br/>
+                            <button onClick={LTHprice} className="sortbutton">low to high (price $USD)</button>
+                            <br/><br/>
+                            <button onClick={HTLchange} className="sortbutton">high to low (change %)</button><br/>
+                            <button onClick={LTHchange} className="sortbutton">low to high (change %)</button>
+                        </pre>
                     </div>
-                    <div id="meritcircle">
-                        <center>
-                        <iframe src="https://treasury.meritcircle.io/" title="treasury" width="100%" height="1325px" scrolling="no" className="treasury--display"></iframe>
-                        </center>
+                    <br/><br/>
+                    <div className="coin-boundbox">
+                        {filteredCoins.map((coin) => {
+                            return (
+                                <>
+                                <Coin
+                                    key={coin.id}
+                                    name={coin.name}
+                                    image={coin.image}
+                                    symbol={coin.symbol}
+                                    price={coin.current_price}
+                                    priceChange={coin.price_change_percentage_24h}
+                                    high24h={coin.high_24h}
+                                    low24h={coin.low_24h}
+                                    maxsupply={coin.max_supply}
+                                    totalsupply={coin.total_supply}
+                                    circsupply={coin.circulating_supply}
+                                    lastupdated={coin.last_updated}
+                                />
+                                </>
+                            );
+                        })}
                     </div>
+                    <br/><br/>
                 </div>
+                <center><iframe src="https://treasury.meritcircle.io/" title="treasury" width="100%" height="1325px" scrolling="no" className="treasury--display"></iframe></center>
+            </div>
         </div>
     )
 };
