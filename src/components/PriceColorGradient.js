46a6337c5930ee0af4ac57ea14cfbbcfae24f58c
324Hz <mark@cosimo.fund>
2022-07-08 04:14:45 -0500

    commit NOTE: finnalyyy got it to change color with an onClick function gRRRrRRR

--- src/components/PriceColorGradient.js
+++ src/components/PriceColorGradient.js
@@ -4,24 +4,24 @@ import "../components/style/pricecolor/pricecolor.css";
 const PriceColorGradient = ({ price, high24h, low24h }) => {
     const [style, setStyle] = useState("cont");
 
-    // function handleTEST() {
-    //    setStyle("contnew");
-    // }
-
-    function COINPRICE() {
-        if (price > low24h) {
-            setStyle("contnew");
+    const changeStyle = () => {
+        if (price < high24h) {
+            setStyle("cont");
+        };
+        if (price === high24h) {
+            setStyle("contnew")
         }
     };
 
-    COINPRICE();
+    // make the function run every time the price changes (MOST LIKELY USING onChange) 
+    // but for now use a onClick function for testing
 
     return (
         <div>
-           
+            <button onClick={changeStyle}>CHECK PRICE COLOR</button>
             <p className={style}>current price: ${price}</p>
-            <p className={high24h}>high (24h): ${high24h}</p>
-            <p className={low24h}>low (24h): ${low24h}</p>
+            <p className="coin-high24h">high (24h): ${high24h}</p>
+            <p className="coin-low24h">low (24h): ${low24h}</p>
         </div>
     );
 };
