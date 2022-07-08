8561dd981daf95273abbd64155b843968752c6d6
324Hz <mark@cosimo.fund>
2022-07-08 05:04:54 -0500

    commit (changed color scheme and worked a bit on the logic for the prices)

--- src/components/PriceColorGradient.js
+++ src/components/PriceColorGradient.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 import "../components/style/pricecolor/pricecolor.css";
 
 const PriceColorGradient = ({ price, high24h, low24h }) => {
-    const [style, setStyle] = useState("cont");
+    const [style, setStyle] = useState("contgreen");
 
     const changeStyle = () => {
         if (price < high24h) {
-            setStyle("cont");
+            setStyle("contgreen");
         };
         if (price === high24h) {
-            setStyle("contnew")
+            setStyle("contred")
+        }
+        if (price === ((low24h + high24h) / 2)+-10) {
+            setStyle("contyellow")
         }
     };
 
-    // make the function run every time the price changes (MOST LIKELY USING onChange) 
-    // but for now use a onClick function for testing
+    /*
+    NOTES:
+     - Try to use the +- operator to compare it the price is within a range to the midpoint of +10 or -10 USD
+     - Make the price color a onChange function so that when the price updates, so does the color
+    */
 
     return (
         <div>
