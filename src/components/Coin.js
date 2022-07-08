a727880163c12ad3e166b0b6ec5d2097012ebfee
324Hz <mark@cosimo.fund>
2022-07-08 10:57:04 -0500

    commit (tried to add logic to the "is near midpoint" code it does NOT work for ksm or hnt for some reason

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,25 +2,15 @@ import React from "react";
 
 const Coin = ({ image, symbol, price, priceChange, high24h, low24h, name, totalsupply, circsupply }) => {
 
+  const testcalc = (high24h + low24h) / 2;
+  const midpointcalc = testcalc;
+
   const pricenew = price.toLocaleString('en-US', {maximumFractionDigits: 2});
   const high24hnew = high24h.toLocaleString('en-US', {maximumFractionDigits: 2});
+  const midpointcalcnew = midpointcalc.toLocaleString('en-US', {maximumFractionDigits: 2});
   const low24hnew = low24h.toLocaleString('en-US', {maximumFractionDigits: 2});
   const totalsupplynew = totalsupply.toLocaleString('en-US', {maximumFractionDigits: 2});
   const circsupplynew = circsupply.toLocaleString('en-US', {maximumFractionDigits: 2});
-
-  // const testcalc = (high24h + low24h) / 2;
-  // const midpointcalc = symbol + " midpoint: " + testcalc;
-
-  // const midpointcalcnew = () => {
-  //   if (price === (midpointcalc + 10) || price === (midpointcalc - 10)) {
-  //     console.log("high");
-  //   } else {
-  //     console.log("low");
-  //   }
-  // };
-
-  // console.log(midpointcalc);
-  // midpointcalcnew();
 
   return (
     <>
@@ -37,9 +27,19 @@ const Coin = ({ image, symbol, price, priceChange, high24h, low24h, name, totals
             <p className="coin-price">price: ${pricenew}</p>
             <p className="coin-high24h">high (24h): ${high24hnew}</p>
             <p className="coin-low24h">low (24h): ${low24hnew}</p>
-            {priceChange < 0 ? (<p className="coin-percent red">change (24h): {priceChange.toFixed(2)}%</p>) : (<p className="coin-percent green">Change (24h): {priceChange.toFixed(2)}%</p>)}
+            {priceChange < 0 ? (<p className="coin-percent red">change (24h): {priceChange.toFixed(2)}%</p>):(<p className="coin-percent green">Change (24h): {priceChange.toFixed(2)}%</p>)}
             <p className="">total supply: {totalsupplynew}</p>
             <p className="">circulating supply: {circsupplynew}</p>
+            <br/>
+            <h1>midpoint details</h1>
+            <h3 className="coin-price">price: ${pricenew}</h3>
+            <h3 className="coin-midpoint">midpoint (24h): ${midpointcalcnew}</h3>
+            { midpointcalcnew < pricenew ? (<p className="midtrue">is above midpoint (24h): true</p>):(<p className="midfalse">is above midpoint (24h): false</p>)}
+            { midpointcalcnew + 10 < pricenew ? (<p className="midtrue">is above midpoint (24h) + 10: true</p>):(<p className="midfalse">is above midpoint (24h) + 10: false</p>)}
+            { midpointcalcnew === pricenew ? (<p className="midtrue">is at midpoint (24h): true</p>):(<p className="midfalse">is at midpoint (24h): false</p>)}
+            { midpointcalcnew + 10 < pricenew || midpointcalcnew - 10 > pricenew ? (<p className="midtrue">is near midpoint (24h): true</p>):(<p className="midfalse">is near midpoint (24h): false</p>)}
+            { midpointcalcnew - 10 > pricenew ? (<p className="midtrue">is below midpoint (24h) - 10: true</p>):(<p className="midfalse">is below midpoint (24h) - 10: false</p>)}
+            { midpointcalcnew > pricenew ? (<p className="midtrue">is below midpoint (24h): true</p>):(<p className="midfalse">is below midpoint (24h): false</p>)}
             <br/>
             </pre>
           </div>
