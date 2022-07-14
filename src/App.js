64f9ac962b5ecdfe93497d7a7cbcff9c7d527f71
324Hz <1024Bitcoins@gmail.com>
2022-07-14 13:29:29 -0500

    commit NOTE: start working on react router rounting to diff pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import MainPage from './MainPage';
+import CoinFormat from './components/CoinFormat';
+import "./components/style/solanabeach/solanabeach.css";
+import "./components/style/main/main.css";
+import "./components/style/coin/coin.css";
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <CoinFormat />
     </div>
   );
 }
