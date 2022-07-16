0ff76b32f618ff5fdf57146c61d4de925d9e4190
324Hz <1024Bitcoins@gmail.com>
2022-07-16 04:10:08 -0500

    commit NOTE styling changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <h1 align="center">Simple crypto dashboard v2</h1>
 
-<h4 align="center">Now you might be asking: Why is there a v2 with almost the exact same code? and to that, I will answer that I have just recently got a new laptop and I was working on moving files and noticed that the ones with node modules took forever to move so I decided to make a new repository with the same code in order to save some time</h4>
+<h4 align="center">This is the new version of the simple crypto dashboard, including things like different pages to navigate to as well as a cleaner formatted solana wallet page</h4>
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,19 +8,19 @@ function Home () {
             <nav className="navbar">
                 <Link to="/components/CoinFormat" className="navbar">
                     <center>
-                        <img className="icons" src="https://media-s3-us-east-1.ceros.com/aon/images/2021/09/14/bf46ac10b25be9533791f580f71e3315/finance-1.png" alt=""/>
+                        <img className="icons-images" src="https://media-s3-us-east-1.ceros.com/aon/images/2021/09/14/bf46ac10b25be9533791f580f71e3315/finance-1.png" alt=""/>
                         <p>coin prices</p>
                     </center>
                 </Link>
                 <Link to="/components/SolanaBeachWalletFormat" className="navbar">
                     <center>
-                        <img className="icons" src="https://cdn1.iconfinder.com/data/icons/banking-3/100/bank-07-512.png" alt=""/>
+                        <img className="icons-images" src="https://cdn1.iconfinder.com/data/icons/banking-3/100/bank-07-512.png" alt=""/>
                         <p>solana wallet</p>
                     </center>
                 </Link>
                 <Link to="/components/MeritCircle" className="navbar">
                     <center>
-                        <img className="icons" src="https://aws1.discourse-cdn.com/standard20/uploads/merit/original/1X/29d50248bf557e113c2529f32dcf2519549b7f30.png" alt=""/>
+                        <img className="icons-images" src="https://aws1.discourse-cdn.com/standard20/uploads/merit/original/1X/29d50248bf557e113c2529f32dcf2519549b7f30.png" alt=""/>
                         <p>merit circle DAO treasury</p>
                     </center>
                 </Link>
