a522b1044b2609de26b7cc81a336a9901ff09b22
324Hz <1024Bitcoins@gmail.com>
2022-07-22 15:36:03 -0500

    commit NOTE: added logo

--- src/components/SolanaBeachMain/FinalSolanaBeachWallet.js
+++ src/components/SolanaBeachMain/FinalSolanaBeachWallet.js
@@ -3,7 +3,9 @@ import SolanaBeachWalletStakeRewards from "./SolanaBeachWalletStakeRewards";
 
 const FinalSolanaBeachWallet = () => {
     return (
-        <>
+        <>  <center>
+                <img src="https://o.remove.bg/downloads/d427ae14-6ee0-425b-a13d-3fadfd37af35/OJgMtfGd_400x400-removebg-preview.png" alt="Solana Beach" className="home-logo" />
+            </center>
             <SolanaBeachWalletBalance/>
             <br/>
             <br/>
